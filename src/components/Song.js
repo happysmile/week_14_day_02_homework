3fb1c86abff3a006d6bccebd64a197543bbf66e2
user <mancusofrancesca@hotmail.com>
2020-04-07 10:44:20 +0100

    fixed audio player

--- src/components/Song.js
+++ src/components/Song.js
@@ -8,9 +8,12 @@ const Song = ({title, releaseDate, artist, category, imageUrl, audioUrl}) => {
       <img src={imageUrl} alt={artist +" - "+ title} />
       <p>Release date: {releaseDate}</p>
       <p>Category: {category}</p>
-      <p><a href={audioUrl}>Audio Preview</a></p>
+      <audio src={audioUrl} controls type="audio/ogg"></audio>
     </li>
   )
 }
 
 export default Song;
+
+// <p><a href={audioUrl}>Audio Preview</a></p>
+//<audio src={} controls type="audio/ogg"></audio>
